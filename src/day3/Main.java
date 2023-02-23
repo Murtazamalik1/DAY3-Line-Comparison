06d2163d5861cb37487741a82514da3e13e1a09b
murtazamalik <murtazamalik1319@gmail.com>
2023-02-23 20:45:22 +0530

    Calculate Length

--- src/day3/Main.java
+++ src/day3/Main.java
@@ -1,8 +1,16 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Welcome To Line Comparison");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter value for x1,y1 and x2,y2 of Line 1");
+        Point p1 = new Point(scanner.nextInt(),scanner.nextInt());
+        Point p2 = new Point(scanner.nextInt(),scanner.nextInt());
+        Line l1 = new Line(p1,p2);
+        float length = l1.getLength();
+        System.out.println(length);
     }
 }
