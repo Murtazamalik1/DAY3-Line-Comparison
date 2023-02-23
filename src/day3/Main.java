8298dada3851c1bf10f6b0e6ae477e4f7aa42b48
murtazamalik <murtazamalik1319@gmail.com>
2023-02-23 21:59:09 +0530

    check equality of two lines

--- src/day3/Main.java
+++ src/day3/Main.java
@@ -10,7 +10,25 @@ public class Main {
         Point p1 = new Point(scanner.nextInt(),scanner.nextInt());
         Point p2 = new Point(scanner.nextInt(),scanner.nextInt());
         Line l1 = new Line(p1,p2);
-        float length = l1.getLength();
-        System.out.println(length);
+        float length1 = l1.getLength();
+        System.out.println(" length of line1 :"+length1);
+
+        System.out.println("Enter the Values for x1,y1 And x2,y2 of Line2  ");
+        Point p3 = new Point(scanner.nextInt(),scanner.nextInt());
+        Point p4 = new Point(scanner.nextInt(),scanner.nextInt());
+        Line l2 = new Line(p3,p4);
+        float length2 = l2.getLength();
+       // System.out.println("length of line1 :" + length1);
+        System.out.println("length of lin2 :" +length2);
+
+        if (l1 == l2){
+            System.out.println("both lines are equal");
+        }
+        else {
+            System.out.println("Lines are not equal");
+        }
+    }
+    static double getLength(double x1, double y1, double x2, double y2 ){
+        return (Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
     }
 }
